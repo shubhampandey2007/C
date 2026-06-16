13d7c1d130cb381ccb88aee1ebeced2ca604df61
Shubham Pandey <shubhamalexyo007@gmail.com>
2026-06-16 09:12:02 +0530

    first commit

---
+++ 2-Comment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//This is for single line comment
+
+/*
+Hello
+Multiple Lines can be written through this
+Each comment can have it's own line
+*/
+int main() {
+    printf("Hello World \n");
+    printf("Practice for Comments \n");
+    printf("Comment consists of 2 types: Single Line and Multiple Line \n");
+    return 0;
+    
+}
+
+
---
+++ 3-Output.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int age = 22;
+    printf("age is %d \n",age);
+    char star = '*';
+    printf("star looks like this %c \n",star);
+    float pi = 3.14;
+    printf("value of pi is %f \n",pi);
+    return 0;
+    }

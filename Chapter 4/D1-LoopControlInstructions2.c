0c830056ac757152ee3951d1e915fa778ed54781
Shubham Pandey <shubhamalexyo007@gmail.com>
2026-07-11 16:57:50 +0530

    CODE

--- Chapter 4/D1-LoopControlInstructions2.c
+++ Chapter 4/D1-LoopControlInstructions2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 //For Loop: Counting till 10
 int main() {
-    for(int i=1; i <= 9; i= i+1) {
+    for(int i=1; i <= 10; i= i+1) {
         printf("%d \n", i);
     }
     return 0;

134e97e50de4c9f2b101b1c026180a5520a4108c
Shubham Pandey <shubhamalexyo007@gmail.com>
2026-07-16 19:00:46 +0530

    CODE

--- Chapter 4/D2-PracticeQs14.c
+++ Chapter 4/D2-PracticeQs14.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 // Q. Print no.s from 0 to n. (n should be entered by the user)
 int main(){ 
-     printf("enter a number: ");
-     int n;
+    int n;
+    printf("enter a number: ");
     scanf("%d",&n);
+    
     for (int i= 0; i <=n; ++i){
         printf("%d \n", i);
-    printf("%d",n );
     }
     return 0;
 }

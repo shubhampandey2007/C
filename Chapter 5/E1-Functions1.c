73a0e5258f54c2e85f1ae83e31e54ea18532d6c3
Shubham Pandey <shubhamalexyo007@gmail.com>
2026-07-17 18:43:47 +0530

    CODE

---
+++ Chapter 5/E1-Functions1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// function prototype(declaration)
+void printHello();
+
+int main(){
+    printHello(); // function call
+    return 0;
+}
+// function definition
+void printHello(){
+    printf("Hello!");
+}
